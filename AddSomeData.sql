90ecb7d7936f28037795fbdd273ddc9d967a6dab
sidorov <sa86@pm.me>
2019-10-05 03:25:45 +1000

    Добавлен скрипт быстрого заполнения базы.

---
+++ AddSomeData.sql
@@ -0,0 +1,8 @@
+INSERT INTO Orders VALUES('04-10-2019 22:23', 0);
+INSERT INTO Orders VALUES('05-10-2019 03:14', 1);
+
+INSERT INTO OrderEntries VALUES('Apple iPhone 11', 2, 1000, 1);
+INSERT INTO OrderEntries VALUES('Apple iPhone 11 Pro', 1, 1200, 1);
+INSERT INTO OrderEntries VALUES('Samsung Galaxy Note', 1, 800, 1);
+
+INSERT INTO OrderEntries VALUES('Beyerdynamic A1', 1, 800, 2);
