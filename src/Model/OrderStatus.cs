9f68ea99ae88f6e7f8b1d95a4329476bfd4d7fc2
sidorov <sa86@pm.me>
2019-10-07 22:51:36 +1000

    Исправлен OrderStatus

--- src/Model/OrderStatus.cs
+++ src/Model/OrderStatus.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RoxosoftSample.Model
+﻿namespace RoxosoftSample.Model
 {
 	public enum OrderStatus : byte
 	{
-		Accepted = 0,
-		AwaitingShipment = 1,
-		Shipped = 2,
-		Completed = 3
+		InProgress = 0,
+		Completed = 1
 	}
 }
